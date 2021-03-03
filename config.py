0ae63ce99eb047af3f315a07ad79e182f3f498bf
Jackson-coder-arch <jacksonikonya@gmail.com>
2021-03-04 00:03:01 +0300

    initialization

---
+++ config.py
@@ -0,0 +1,33 @@
+import os
+
+
+
+class Config:
+
+    
+    SECRET_KEY = '12345'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://jack:jack@localhost/pitch4'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+class ProdConfig(Config):
+    pass
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://jack:jack@localhost/pitch4'
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
---
+++ start.sh
@@ -0,0 +1,6 @@
+export MAIL_USERNAME="jacksonikonya@gmail.com"
+export MAIL_PASSWORD="Chanukahfashion20"
+export DATABASE_URL='postgresql+psycopg2://jack:jack@localhost/pitch4'
+
+python3.9 manage.py runserver
+
