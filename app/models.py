439274ce4989838df0e599ddbcc81f010bfe5e07
Jackson-coder-arch <jacksonikonya@gmail.com>
2021-03-04 08:23:06 +0300

    index adjustment

--- app/models.py
+++ app/models.py
@@ -5,35 +5,35 @@ from . import login_manager
 from datetime import datetime
 
 
-# class User(UserMixin,db.Model):
-#     __tablename__ = 'users'
+class User(UserMixin,db.Model):
+    __tablename__ = 'users' 
 
-#     id = db.Column(db.Integer,primary_key = True)
-#     username = db.Column(db.String(255),index = True)
-#     email = db.Column(db.String(255),unique = True,index = True)
-#     bio = db.Column(db.String(255))
-#     profile_pic_path = db.Column(db.String())
-#     pass_secure = db.Column(db.String(255))
-#     pitches = db.relationship('Pitch',backref = 'user',lazy="dynamic")
-#     comments = db.relationship('Comment',backref = 'user',lazy="dynamic")
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pass_secure = db.Column(db.String(255))
+    pitches = db.relationship('Pitch',backref = 'user',lazy="dynamic")
+    comments = db.relationship('Comment',backref = 'user',lazy="dynamic")
 
-#     @property
-#     def password(self):
-#         raise AttributeError('You cannot read the password attribute')
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
 
-#     @password.setter
-#     def password(self, password):
-#         self.pass_secure = generate_password_hash(password)
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
         
-#     @login_manager.user_loader
-#     def load_user(user_id):
-#         return User.query.get(int(user_id))
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
-#     def verify_password(self,password):
-#         return check_password_hash(self.pass_secure,password)
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
 
-#     def __repr__(self):
-#         return f'User {self.username}'
+    def __repr__(self):
+        return f'User {self.username}'
 
 class Pitch(db.Model):
     __tablename__ = 'pitches'
