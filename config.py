4542bf4419b0c0f9a525d86036428edc6a605e9a
Jackson-coder-arch <jacksonikonya@gmail.com>
2021-03-04 08:14:04 +0300

    config changes

--- config.py
+++ config.py
@@ -1,33 +1,33 @@
-import os
+# import os
 
 
 
-class Config:
+# class Config:
 
     
-    SECRET_KEY = '12345'
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://jack:jack@localhost/pitch4'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     SECRET_KEY = '12345'
+#     # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://jack:jack@localhost/pitch4'
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    UPLOADED_PHOTOS_DEST ='app/static/photos'
+#     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
-    #  email configurations
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-
-
-class ProdConfig(Config):
-    pass
-    SQLALCHEMY_DATABASE_URL=os.environ.get("DATABASE_URL")
+#     #  email configurations
+#     MAIL_SERVER = 'smtp.googlemail.com'
+#     MAIL_PORT = 587
+#     MAIL_USE_TLS = True
+#     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+#     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
-class DevConfig(Config):
-    DEBUG = True
+# class ProdConfig(Config):
+#     pass
+#     SQLALCHEMY_DATABASE_URL=os.environ.get("DATABASE_URL")
 
-config_options = {
-'development':DevConfig,
-'production':ProdConfig
-}
+
+# class DevConfig(Config):
+#     DEBUG = True
+
+# config_options = {
+# 'development':DevConfig,
+# 'production':ProdConfig
+# }
